77cde4a554f9b4677b48c0e27df20382c102bbda
wagner-victorops <wagner@victorops.com>
2018-07-25 13:05:35 -0600

    update get_admins script

--- get_admins.sh
+++ get_admins.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-INPUT=$1
-
-QUERY="select s.first, s.last, u.email as 'Email' from UserEmails u join OrganizationMemberRoles m on u.user_id = m.user_id join Organizations o on o.id = m.organization_id join Users s on s.id = u.user_id where m.role = 'admin' and o.slug = '$INPUT' group by u.email"
-
-
-mysql -h coredb1.pr.den02.victorops.net -D vo_hier -u rouser -p -e "$QUERY"
+for arg
+do	QUERY="select e.email as 'Email' from UserEmails e join OrganizationMemberRoles m on e.user_id = m.user_id join Organizations o on o.id = m.organization_id join Users u on u.id = e.user_id where m.role = 'admin' and o.slug = '$arg' group by e.email"
+	echo $'-----------------------------------------------------------\n'
+	printf "Admin emails for slug: %s" "$arg"
+	echo $'\n-----------------------------------------------------------'
+	mysql -h coredb1.pr.den02.victorops.net -D vo_hier -u rouser -p -e "$QUERY" | while read Email ; do
+	echo "$Email"
+	done
+	echo $'\n\n'
+done

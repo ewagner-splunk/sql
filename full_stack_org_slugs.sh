dec40aab0d77b5769480330a374e734aab29091d
wagner-victorops <wagner@victorops.com>
2018-10-05 11:21:08 -0600

    New scripts to pull list of orgs by tier (full_stack, essentials, free)

---
+++ full_stack_org_slugs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+QUERY="select o.slug from Organizations o join OrganizationBilling b on b.organization_id = o.id where b.tier = \"enterprise\""
+
+mysql -h coredb1.pr.den02.victorops.net -D vo_hier -u rouser -p -e "$QUERY" | while read org_slug; do
+	echo \"$org_slug\",
+done

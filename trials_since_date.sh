35221f844271ee21b22aa32cd9bfb9f8285ec3b3
wagner-victorops <wagner@victorops.com>
2018-10-10 17:54:38 -0600

    make verbose

--- trials_since_date.sh
+++ trials_since_date.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-INPUT = $1
+INPUT=$1
 
 QUERY="select o.slug from Organizations o join OrganizationBilling b on b.organization_id = o.id where b.state = 'trial' and o.created_on > '$INPUT'"
 
-mysql -h coredb1.pr.den02.victorops.net -D vo_hier -u rouser -p -e "$QUERY" | while read org_slug; do
+mysql -h coredb1.pr.den02.victorops.net -D vo_hier -u rouser -p -v -v -e "$QUERY" | while read org_slug; do
 	echo \"$org_slug\",
 done
